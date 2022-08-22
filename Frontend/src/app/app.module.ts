b96e8bd6f45a042348f3af685178d7ef382f98df
Biju <biju245@gmail.com>
2022-08-22 19:52:24 +0530

    Angular front end theme settings for all entities

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+import { EmployerComponent } from './employer/employer.component';
+import { EmpLoginComponent } from './emp-login/emp-login.component';
+import { EmpSignupComponent } from './emp-signup/emp-signup.component';
+import { StudentComponent } from './student/student.component';
+import { StudLoginComponent } from './stud-login/stud-login.component';
+import { StudSignupComponent } from './stud-signup/stud-signup.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdmnDashboardComponent } from './admn-dashboard/admn-dashboard.component';
+import { AdmnLoginComponent } from './admn-login/admn-login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+    EmployerComponent,
+    EmpLoginComponent,
+    EmpSignupComponent,
+    StudentComponent,
+    StudLoginComponent,
+    StudSignupComponent,
+    AdminComponent,
+    AdmnDashboardComponent,
+    AdmnLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
